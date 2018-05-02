7d6fd25a8863372a271463794894522645567093
Simon-Melia <sibb132@hotmail.co.uk>
2018-05-02 22:03:45 +0100

    imporved exp

--- exp.py
+++ exp.py
@@ -1,6 +1,18 @@
 from test import Test
-class exp(object):
+
+class Exp(object):
+
 	def __init__(self, name):
 		self.person = Test(name)
+
 	def talk(self):
 		self.person.who()
+
+
+def main():
+	simon = Exp('simon')
+	simon.talk() 
+
+
+if __name__ == "__main__":
+	main()
